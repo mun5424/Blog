8d2b81045525ebf8d2e6164c460540f666ff980b
Charlie Oh <mun5424@gmail.com>
2021-01-03 13:36:48 -0800

    refactor files and routing

--- Server/main/app.js
+++ Server/main/app.js
@@ -1,8 +1,12 @@
+//initializations (error handling, express engine, directory handling, parser, logger)
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+//handle routing
+var indexRouter = require('./routes')
 
 var app = express();
 
@@ -12,5 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//cant use routing without
+app.use('/', indexRouter)
 
 module.exports = app;
